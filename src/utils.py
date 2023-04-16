788236f6dfddc93d932d68911b81525b085fb5a3
quanzw99 <carry_quan@163.com>
2023-04-16 00:13:03 -0400

    add evaluation: node classification

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,35 @@
 import json
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
 
 def get_dateset(data_name='wiki'):
     with open('../tests/dataset.json', 'r') as dataset_file:
         dataset_data = json.load(dataset_file)
         return dataset_data[data_name]
+
+def get_labels(filename, skip_head = False):
+    labels = {}
+    with open(filename, 'r') as reader:
+        for line in reader:
+            parts = line.strip().split()
+            labels[parts[0]] = int(parts[1])
+    return labels
+
+def node_classification(embeddings, labels, test_size):
+    keys = sorted(embeddings.keys())
+    X = np.array([embeddings[node] for node in keys])
+    Y = np.array([labels[node] for node in keys])
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+
+    clf = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1000)
+    clf.fit(X_train, Y_train)
+
+    # Test Accuracy
+    print("Test score: {:.5f}".format(clf.score(X_test, Y_test)))
+
+    # Get report
+    Y_pred = clf.predict(X_test)
+    print(classification_report(Y_test, Y_pred, zero_division=1))
+
--- tests/test_deepwalk.py
+++ tests/test_deepwalk.py
@@ -1,8 +1,11 @@
 import sys
 sys.path.append("..")
 from src.utils import get_dateset
+from src.utils import get_labels
+from src.utils import node_classification
 from src import DeepWalk
 import networkx as nx
+
 data_info = get_dateset(data_name='wiki')
 
 if __name__ == "__main__":
@@ -10,3 +13,5 @@ if __name__ == "__main__":
     model = DeepWalk(G, walk_num=10, walk_len=10, walkers=1)
     model.train()
     embeddings = model.get_embedding()
+    labels = get_labels(data_info['labels'])
+    node_classification(embeddings, labels, 0.2)
